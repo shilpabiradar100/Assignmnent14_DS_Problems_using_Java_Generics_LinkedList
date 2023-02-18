c7589d71983631f0e41da54ec17513d5df3c8550
shilpa123 <shilpabiradar100@gmail.com>
2023-02-18 12:11:53 +0530

    ADDED:created a linked list by appending nodes

--- src/linkedlist/LinkedListMain.java
+++ src/linkedlist/LinkedListMain.java
@@ -8,12 +8,18 @@ public class LinkedListMain {
         linkedListObj.add(30);
         linkedListObj.add(70);
         linkedListObj.display();
-        //Here we can get sequence i.e 56 -> 30 ->70
         System.out.println("Using add method");
         LinkedListService<Integer>linkedListObj1 = new LinkedListService<Integer>();
         linkedListObj1.add(70);
         linkedListObj1.add(30);
         linkedListObj1.add(56);
         linkedListObj1.display();
+        System.out.println("Using append method");
+        LinkedListService<Integer>linkedListObj2 = new LinkedListService<Integer>();
+        linkedListObj2.append(56);
+        linkedListObj2.append(30);
+        linkedListObj2.append(70);
+        linkedListObj2.display();
+
     }
 }
--- src/linkedlist/LinkedListService.java
+++ src/linkedlist/LinkedListService.java
@@ -3,13 +3,31 @@ package linkedlist;
 public class LinkedListService<T> {
 
     private Node<T> head;
-    //Method to add nodes at front in the linked list
+    //Method to add nodes at the front in the linked list
     public void add(T data)
     {
         Node<T> newNode = new Node<T>(data);
         newNode.data = data;
         newNode.next = head;
         head = newNode;
+    }
+    //Method to append nodes at rear in the linked list
+    public void append(T data)
+    {
+        Node<T> newNode = new Node<T>(data);
+        if(head == null)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node<T> tempNode = head;
+            while(tempNode.next != null)
+            {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = newNode;
+        }
     }
     //Method to display linked list
     public void display()
